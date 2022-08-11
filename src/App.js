5f216059828549e51dbedbd3b39c938b4551b37b
sniperdev <62942349+sniperdev@users.noreply.github.com>
2022-08-11 16:53:45 +0200

    change the concept of website

--- src/App.js
+++ src/App.js
@@ -1,62 +1,104 @@
 import './App.css';
 import React from "react";
 
-import { WiDaySunny } from "weather-icons-react";
-import WeekWeather from "./components/WeekWeather";
+import {WiCelsius} from "weather-icons-react";
 
 class App extends React.Component {
   state={
+    city:'Poznan',
+    date:'',
     chmury:'',
     temp:'',
     wiatr:'',
-    date:'',
     feelsLike:'',
   }
+
   fetchWeather=()=>{
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=oborniki&exclude=daily&appid=0108d9940c3664c932faecc5a0ef2ab8&units=metric&lang=pl')
       .then(response => response.json())
       .then(json => {
-        console.log(json);
-        const current=new Date();
-        const date=`${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
         this.setState({
           main: json.list[0].weather[0].main,
           temp: json.list[0].main.temp,
           wiatr: json.list[0].wind.speed,
-          date: date,
           feelsLike: json.list[0].main.feels_like,
         })})
   }
+
+  getDayName(){
+    const currentTime=new Date();
+
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+
+    const day = currentTime.getDay();
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    const month = currentTime.getMonth();
+
+    this.setState({
+      date: `${hours}:${minutes} - ${days[day]}, ${currentTime.getDate()} ${months[month]}`,
+    })
+  }
+
+
+  // renderSwitch(){
+  //   let setSize= 40;
+  //   switch({
+  //     case 'Clear':
+  //       return <WiDaySunny size={setSize}/>
+  //     case 'Clouds':
+  //       return <WiCloudy size={setSize}/>
+  //     case 'Rain':
+  //       return <WiDayRain size={setSize}/>
+  //     case 'Drizzle':
+  //       return <WiDayShowers size={setSize}/>
+  //     case 'Thunderstorm':
+  //       return <WiDayThunderstorm size={setSize}/>
+  //     default:
+  //       return <WiDaySunny size={setSize}/>
+  //   }}
+
+
   componentDidMount() {
     this.fetchWeather();
+    this.getDayName();
   }
 
   render(){
     return(
-      <section className={"w-screen h-screen grid grid-cols-3 grid-rows-2"}>
-        <section className={"row-span-2"}>
-          <div>
-            <h1>Oborniki, PL</h1>
-            <h2>{this.state.temp}</h2>
-            <h3>{this.state.main}</h3>
-            <h3>{this.state.date}</h3>
-          </div>
-          <div>
-            <div className={'border-2 bg-neutral-100'}>
-              <h1>{this.state.feelsLike}</h1>
-              <WiDaySunny size={24} color='#000' />
-              <h1>Feels like</h1>
+      <section className={"h-screen flex text-white backgroundImg font-mono"}>
+        <section className={"self-end w-2/3"}>
+          <section className={"flex justify-center"}>
+            <div>
+              <span>{Math.round(this.state.temp)}</span>
+              <WiCelsius size={30} className={"inline"}/>
             </div>
-          </div>
+            <div>
+              <h1>{this.state.city}</h1>
+              <h1>{this.state.date}</h1>
+            </div>
+            <div>
+              <h1>Ikona</h1>
+              <h1>Jaka pogoda</h1>
+            </div>
+          </section>
+          <section>
+            <h1>Hourly forecast</h1>
+            <section className={"flex"}>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </section>
+          </section>
         </section>
-        <section className={"col-span-2 flex bg-neutral-300 rounded-xl"}>
-          <WeekWeather date={"10/08/22"} temp={this.state.temp} feelsLike={this.state.feelsLike} main={this.state.main}/>
-          <WeekWeather date={"10/08/22"} temp={this.state.temp} feelsLike={this.state.feelsLike} main={this.state.main}/>
-          <WeekWeather date={"10/08/22"} temp={this.state.temp} feelsLike={this.state.feelsLike} main={this.state.main}/>
-          <WeekWeather date={"10/08/22"} temp={this.state.temp} feelsLike={this.state.feelsLike} main={this.state.main}/>
+        <section className={"w-1/3"}>
+          MIASTO I INNE
         </section>
-        <section className={"col-span-2"}></section>
       </section>
+
     )
   }
 }
